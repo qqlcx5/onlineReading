99ab9e324b5dfeb7908ece7b01d7f0d5bb12aa5c
“qqlcx5” <qqlcx5@gmail.com>
2020-05-01 00:55:51 +0800

    项目精简

--- vue.config.js
+++ vue.config.js
@@ -81,7 +81,8 @@ module.exports = {
       .end()
 
     config
-      .when(process.env.NODE_ENV !== 'development',
+      .when(process.env.NODE_ENV === 'development',
+        config => config.devtool('cheap-source-map'),
         config => {
           config
             .plugin('ScriptExtHtmlWebpackPlugin')
