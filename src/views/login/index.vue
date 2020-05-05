a70e90c3ae4c579429c53ba37dac70d1dd8ebcaa
qqlcx5 <893917884@qq.com>
2020-05-05 16:59:50 +0800

    登录校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -61,21 +61,20 @@
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
 
 export default {
   name: 'Login',
   data() {
     const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+      if (!value && value.length === 0) {
+        callback(new Error('请输入用户名'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+      if (value.length < 4) {
+        callback(new Error('请输入4位的密码'))
       } else {
         callback()
       }
